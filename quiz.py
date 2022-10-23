85a5fc62ef0b1ff80a74695088b8cd296dcb5f3e
DarkValkier <darkvalkier@gmail.com>
2022-10-23 12:41:51 +0300

    Добавлен вопрос про области

--- quiz.py
+++ quiz.py
@@ -15,3 +15,19 @@ else:
     print('Неверно!')
 
 print('Правильных ответов', points)
+
+print('На сколько областей делится территория Украины?')
+print('а) 20')
+print('б) 23')
+print('в) 25')
+print('г) 19')
+
+answer = input('Ваш ответ: ').lower()
+
+if answer == 'в':
+    print('Верно!')
+    points += 1
+else:
+    print('Неверно!')
+
+print('Правильных ответов', points)
