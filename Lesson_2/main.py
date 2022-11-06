808d5ca8bce3aaf7fd55374724a3450ba6c89f02
DarkValkier <darkvalkier@gmail.com>
2022-11-06 14:37:55 +0200

    Добавлен цикл боя насмерть

--- Lesson_2/main.py
+++ Lesson_2/main.py
@@ -6,8 +6,9 @@ player2 = Character(name='Petya', health=30, damage=2, defence=4)
 print(player1)
 print(player2)
 
-player1.attack(player2)
-player2.attack(player1)
+while player1.health > 0 and player2.health > 0:
+    player1.attack(player2)
+    player2.attack(player1)
 
-print(player1)
-print(player2)
+    print(player1)
+    print(player2)
